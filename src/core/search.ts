d606f80bcc2af39700ffd2a2fbb42622fc17213e
saber2pr <saber2pr@gmail.com>
2019-02-14 17:12:14 +0800

    update

--- src/core/search.ts
+++ src/core/search.ts
@@ -2,10 +2,10 @@
  * @Author: saber2pr
  * @Date: 2019-01-31 20:26:02
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-01-31 20:34:14
+ * @Last Modified time: 2019-02-14 17:11:50
  */
 export const deepSearch = (
-  array: Object[],
+  array: { [k: string]: any }[],
   callback: (value: string) => void
 ): void =>
   array.forEach(obj =>
